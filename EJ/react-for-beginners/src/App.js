834b211ca6772c70e0b8f52d9e7fa16582767456
kej <publdaze@naver.com>
2022-01-14 21:06:50 +0900

    #8 7.0실습

--- EJ/react-for-beginners/src/App.js
+++ EJ/react-for-beginners/src/App.js
@@ -1,26 +1,30 @@
-import { useState, useEffect } from "react";
-
-function Hello() {
-  useEffect(function () {
-    console.log("hi :)");
-    return function () {
-      console.log("bye :(");
-    };
-  }, []);
-  useEffect(() => {
-    console.log("hi :)");
-    return () => console.log("bye :(");
-  }, []);
-  return <h1>Hello</h1>;
-}
+import { useState } from "react";
 
 function App() {
-  const [showing, setShowing] = useState(false);
-  const onClick = () => setShowing((prev) => !prev);
+  const [toDo, setToDo] = useState("");
+  const [toDos, setToDos] = useState([]);
+  const onChange = (event) => setToDo(event.target.value);
+  const onSubmit = (event) => {
+    event.preventDefault(); // 없애면 새로 고침 됨.
+    if (toDo === "") {
+      return;
+    }
+    setToDo(""); // 제출했을 때 비워지도록 함.
+    setToDos((currentArray) => [toDo, ...currentArray]);
+  };
+  console.log(toDos);
   return (
     <div>
-      {showing ? <Hello /> : null}
-      <button onClick={onClick}>{showing ? "Hide" : "Show"}</button>
+      <h1>My to Dos ({toDos.length})</h1>
+      <form onSubmit={onSubmit}>
+        <input 
+          onChange={onChange}
+          value={toDo}
+          type="text" 
+          placeholder="Write your to do..." 
+        />
+        <button> Add </button>
+      </form>
     </div>
   );
 }
