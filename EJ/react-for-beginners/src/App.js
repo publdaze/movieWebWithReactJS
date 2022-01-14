4ace149139ce52a7d78f9e81bb8e70df994f480a
kej <publdaze@naver.com>
2022-01-14 17:39:10 +0900

    #7 6.0 실습

--- EJ/react-for-beginners/src/App.js
+++ EJ/react-for-beginners/src/App.js
@@ -1,11 +1,13 @@
-import Button from "./Button";
 import styles from "./App.module.css";
+import { useState } from "react";
 
 function App() {
+  const [counter, setValue] = useState(0);
+  const onClick = () => setValue((prev) => prev + 1);
   return (
     <div>
-      <h1 className={styles.title}>Welcome</h1>
-      <Button text={"Continue"} />
+      <h1 className={styles.title}>{counter}</h1>
+      <button onClick={onClick}>click me</button>
     </div>
   );
 }
