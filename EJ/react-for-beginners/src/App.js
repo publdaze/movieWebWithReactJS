c29b67fd7f4035e3eec75e8cb4344522ae95d37b
kej <publdaze@naver.com>
2022-01-14 17:47:35 +0900

    #7 6.1 실습

--- EJ/react-for-beginners/src/App.js
+++ EJ/react-for-beginners/src/App.js
@@ -1,12 +1,15 @@
-import styles from "./App.module.css";
-import { useState } from "react";
+import { useState, useEffect } from "react";
 
 function App() {
   const [counter, setValue] = useState(0);
   const onClick = () => setValue((prev) => prev + 1);
+  console.log("i run all the time");
+  useEffect(() => {
+    console.log("CALL THE API....");
+  }, []);
   return (
     <div>
-      <h1 className={styles.title}>{counter}</h1>
+      <h1>{counter}</h1>
       <button onClick={onClick}>click me</button>
     </div>
   );
