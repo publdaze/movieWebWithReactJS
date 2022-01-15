2cc70793ff875687cd759e6d279d1742f1368a3b
kej <publdaze@naver.com>
2022-01-15 11:06:25 +0900

    #8 7.2 실습

--- EJ/react-for-beginners/src/App.js
+++ EJ/react-for-beginners/src/App.js
@@ -1,34 +1,28 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 
 function App() {
-  const [toDo, setToDo] = useState("");
-  const [toDos, setToDos] = useState([]);
-  const onChange = (event) => setToDo(event.target.value);
-  const onSubmit = (event) => {
-    event.preventDefault(); // 없애면 새로 고침 됨.
-    if (toDo === "") {
-      return;
-    }
-    setToDo(""); // 제출했을 때 비워지도록 함.
-    setToDos((currentArray) => [toDo, ...currentArray]);
-  };
-  console.log(toDos);
-  return (
+  const [loading, setLoading] = useState(true);
+  const [coins, setCoins] = useState([]);
+  useEffect(() => {
+    fetch("https://api.coinpaprika.com/v1/tickers")
+    .then((response) => response.json())
+    .then((json) => {
+      setCoins(json);
+      setLoading(false);
+    });
+  }, [])
+  return(
     <div>
-      <h1>My to Dos ({toDos.length})</h1>
-      <form onSubmit={onSubmit}>
-        <input 
-          onChange={onChange}
-          value={toDo}
-          type="text" 
-          placeholder="Write your to do..." 
-        />
-        <button> Add </button>
-      </form>
-      <hr />
-      <ul>
-        {toDos.map((item, index) => <li key={index}>{item}</li>)}
-      </ul>
+      <h1>The Coins! {loading ? "" : `(${coins.length})`}</h1>
+      {loading ? <strong>Loading...</strong> : (
+        <select>
+          {coins.map((coin) => (
+            <option key={coin.id}>
+              {coin.name} ({coin.symbol}): ${coin.quotes.USD.price} USD
+            </option>
+          ))}
+        </select>
+      )}
     </div>
   );
 }
